ab3864df46a2c48a0d1f7c75457f8e2d20b16fb0
zachschool1 <zacharyjordan@my.whccd.edu>
2026-02-07 09:51:42 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace COMP003A.Assignment4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+            Console.WriteLine("test;");
+        }
+    }
+}
