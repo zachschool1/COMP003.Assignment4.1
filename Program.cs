e3781c472746e362bb6bc1db1952b14f841a6f10
zachschool1 <zacharyjordan@my.whccd.edu>
2026-02-07 17:38:39 -0800

    prints twice for some reason

--- Program.cs
+++ Program.cs
@@ -1,16 +1,42 @@
-﻿namespace COMP003A.Assignment4._1
+﻿using System.Diagnostics.Metrics;
+
+namespace COMP003A.Assignment4._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int stepCounter = 0;
+            char response;
+            do
+            {
+                Console.WriteLine("Pick a choice: ");
+                Console.WriteLine("1. Start/Continue");
+                Console.WriteLine("2. View Progress");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Enter Choice: ");
+                response = Console.ReadKey().KeyChar;
+                Console.WriteLine();
 
 
+                if (response == '3')
+                {
+                    Console.WriteLine("Exiting Program");
+                    break;
+                }
 
-            Console.WriteLine("test;");
+                if (response == '1')
+                {
+                    stepCounter++;
+                    Console.WriteLine($"Routine step {stepCounter} completed");
+                }
+                if (response == '2')
+                {
+                    Console.WriteLine($"Current progress: {stepCounter} steps completed.");
+                }
+            } 
+            while (true);
 
-            Console.WriteLine("test2");
         }
     }
 }
